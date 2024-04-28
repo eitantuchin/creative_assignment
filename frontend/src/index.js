dec468a77069bee0a57dfedbd3dd3ebc783bd4d4
Eitan <eitantuchin@gmail.com>
2024-04-28 16:17:08 -0500

    Setting up stuff pt 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './navbar';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import AddProductView from './addProduct';
-import DeleteProductView from './deleteProduct';
-import UpdateProductView from './updateProduct';
-import StudentInfo from './studentInfo';
-import Store from './fakestore';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/store" element={<Store />} />
-        <Route path="/create-product" element={<AddProductView />} />
-        <Route path="/update-product" element={<UpdateProductView />} />
-        <Route path="/delete-product" element={<DeleteProductView />} />
-        <Route path="/student-info" element={<StudentInfo />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
